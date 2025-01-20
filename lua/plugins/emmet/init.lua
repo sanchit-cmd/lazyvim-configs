19c4e00cbc72c73ee19b5dea1d31344b70b88264
Sanchit Nigam <sanchit.nigam2005@gmail.com>
2025-01-20 10:26:45 +0530

    Initial commit of custom LazyVim configurations

---
+++ lua/plugins/emmet/init.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        emmet_ls = {
+          filetypes = {
+            "html",
+            "typescriptreact",
+            "javascriptreact",
+            "css",
+            "sass",
+            "scss",
+            "less",
+            "javascript",
+            "typescript",
+            "markdown",
+            "ejs",
+          },
+          init_options = {
+            html = {
+              options = {
+                ["bem.enabled"] = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
